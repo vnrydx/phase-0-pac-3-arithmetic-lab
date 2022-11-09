2b1e05b0958ea27544c50398c7e1f543efe968e6
vnrydx <vnrydx@gmail.com>
2022-11-09 17:06:34 -0600

    commit tree fc68da9db4bb182878f2f1af3ac8dd3d6480609b

---
+++ index.js
@@ -0,0 +1,31 @@
+function add(a, b) {
+    return a + b;// hey
+}
+
+function subtract(a, b) {
+    return a - b;// hey
+}
+
+function multiply(a, b) {
+    return a * b;// hey
+}
+
+function divide(a, b) {
+    return a / b;// hey
+}
+
+function increment(n) {
+    return n += 1
+}
+
+function decrement(n) {
+    return n -= 1;// hey
+}
+
+function makeInt(string) {
+    return parseInt(string, 10);
+}
+
+function preserveDecimal(string) {
+    return parseFloat(string);
+}
